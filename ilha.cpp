b18852a05f853148a700078adcc5d62aaf8128f3
kylixalex <kylixsilva@gmail.com>
2021-11-16 00:54:16 +0000

    Make ilha generateable

--- ilha.cpp
+++ ilha.cpp
@@ -1,6 +1,13 @@
 #include "ilha.h"
+#include "zona.h"
+
+using namespace std;
 
 Ilha::Ilha(int linhas, int colunas)
 {
-	vector<vector<string>> zonas(linhas, vector<string> (colunas, "a"));
+	vector<vector<Zona>> zonas(colunas);
+	for (int i = 0; i < colunas; i++)
+	{
+		zonas[i].resize(linhas);
+	}
 }
--- zona.h
+++ zona.h
@@ -5,7 +5,12 @@ class Zona {
 	private:
 		string zone_type;
 	public:
+	/*
 		Zona(string zone_type);
+
+		Have to leave this default constructor because
+		otherwise I cannot create a vector of vectors of this type.
+	*/
 };
 
 #endif // ZONA_H
