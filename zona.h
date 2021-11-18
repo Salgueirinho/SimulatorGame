76481d5d53bc10313691d0d317e01c1a3f41becf
kylixalex <kylixsilva@gmail.com>
2021-11-18 23:14:10 +0000

    Add inheritance for different zone classes

--- zona.h
+++ zona.h
@@ -9,16 +9,40 @@ using namespace std;
 class Zona {
 	private:
 		string zone_type;
-    int eficiencia_mineracao;
+    static	int eficiencia_mineracao;
 	public:
 		void setZone(string zone_type);
 		string getZone(void) const;
-	/*
-		Zona(string zone_type);
+};
 
-		Have to leave this default constructor because
-		otherwise I cannot create a vector of vectors of this type.
-	*/
+class Pastagem : public Zona {
+	private:
+	public:
+};
+
+class	Deserto : public Zona {
+	private:
+	public:
+};
+
+class Floresta : public Zona {
+	private:
+	public:
+};
+
+class Montanha : public Zona {
+	private:
+	public:
+};
+
+class Pantano : public Zona {
+	private:
+	public:
+};
+
+class ZonaX : public Zona {
+	private:
+	public:
 };
 
 #endif // ZONA_H
