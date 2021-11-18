99ae1b422bf650c39a3a0a26badf3059d109fd62
kylixalex <kylixsilva@gmail.com>
2021-11-18 23:25:08 +0000

    Add time

---
+++ dia.h
@@ -0,0 +1,11 @@
+#ifndef DIA_H
+#define DIA_H
+
+class	Dia {
+	private:
+		int	dia;
+		string	momento;
+	public:
+};
+
+#endif // DIA_H
---
+++ tempo.h
@@ -0,0 +1,11 @@
+#ifndef DIA_H
+#define DIA_H
+
+class	Tempo {
+	private:
+		int	dia;
+		int	hora;
+	public:
+};
+
+#endif // DIA_H
