3a621b39505a70e63979be91c00131c0b631fc95
kylixalex <kylixsilva@gmail.com>
2021-11-29 18:13:05 +0000

    Revert "Add look up table and map containing look up table"
    This reverts commit e394355215a2d6ed5f50a6fdd392de9b49d4946f.

--- Interface.cpp
+++ Interface.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <fstream>
 #include "Interface.h"
 #include "Island.h"
 #include "utils.h"
 
-void	Interface::start()
+void	Interface::start() const
 {
 	std::string option; 
 
@@ -23,33 +22,14 @@ void	Interface::start()
 	std::cout << "See you next time!" << std::endl;
 }
 
-void	Interface::game()
+void	Interface::game() const
 {
 	std::string	command;
 
 	Island	island(getNumberBetween(3, 8, "Rows for the island, please: "), getNumberBetween(3, 16, "Columns for the island, please: "));
-	this->createLookUpTable();
 	while (command.compare(0, command.find(' '), "exit"))
 	{
 		island.displayZones();
 		command = getCommand({"exit"}, "Command: ");
 	}
-}
-
-void	Interface::createLookUpTable()
-{
-	std::fstream	fs;
-	std::string	buffer;
-	fs.open("lookuptable");
-	if (!fs)
-	{
-		std::cout << "Unable to open file\"lookuptable\"." << std::endl;
-		return ;
-	}
-	while (!fs.eof())
-	{
-		std::getline(fs, buffer);
-		lut.insert(std::pair<std::string, std::string>(buffer.substr(0, buffer.find(' ')), buffer.substr(buffer.find(' ') + 1, buffer.size())));
-	}
-	fs.close();
 }
--- Interface.h
+++ Interface.h
@@ -1,15 +1,10 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
-#include <string>
-#include <map>
-
 class	Interface {
 	public:
-		void	start();
-		void	game();
-		void	createLookUpTable();
-		std::map<std::string, std::string>	lut;
+		void	start() const;
+		void	game() const;
 	private:
 };
 
