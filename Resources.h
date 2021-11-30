b3d6c1e37d9868c737ca03da03d1aabd4c67558f
kylixalex <kylixsilva@gmail.com>
2021-11-30 19:10:31 +0000

    Add resources

---
+++ Resources.h
@@ -0,0 +1,16 @@
+#ifndef RESOURCES_H
+#define RESOURCES_H
+
+class Resources {
+	public:
+	private:
+		int	iron;
+		int	steel;
+		int	coal;
+		int	wood;
+		int	woodPlanks;
+		int	electricity;
+		int	cash;
+};
+
+#endif
